0b3a3ed62b600ec51ba935fb8a6ca83d9969c8a6
Sai Bharadwaj Kamrajugadda <148251061+Bharatsolo@users.noreply.github.com>
2026-03-13 22:15:24 +0530

    fixed bugs and issues

--- src/test/java/com/revshop/catalogservice/service/ProductServiceTest.java
+++ src/test/java/com/revshop/catalogservice/service/ProductServiceTest.java
@@ -21,7 +21,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
